f6c67321b28cf289e6ace9fc8e72579a9cc19a01
KTrain5369 <69028025+KTrain5169@users.noreply.github.com>
2024-10-20 20:46:10 +1000

    rewrite pt 2

---
+++ src/astro.d.ts
@@ -0,0 +1,26 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    div: any;
+    h1: any;
+    h2: any;
+    p: any;
+    a: any;
+    main: any;
+    button: any;
+    img: any;
+    span: any;
+    header: any;
+    footer: any;
+    nav: any;
+    li: any;
+    ul: any;
+    html: any;
+    head: any;
+    meta: any;
+    title: any;
+    link: any;
+    slot: any;
+    body: any;
+    // Add other elements as necessary
+  }
+}
