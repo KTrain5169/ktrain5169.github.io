9dfba910b7666c089f6b17ffadf726be54a430f3
KTrain5369 <git@ktrain5369.me>
2024-11-22 22:13:48 +1000

    begin migrating blog and project pages to astro:content

--- src/content/blog/test-blog.md
+++ src/content/blog/test-blog.md
@@ -1,5 +1,4 @@
 ---
-layout: ../../layouts/BlogPostLayout.astro
 title: "Test blog post"
 pubDate: 2024-11-19
 description: "Just testing out Astro's blog functionality with .md"
@@ -8,7 +7,5 @@ tags: ["astro", "blog", "test"]
 ---
 
 # Test blog post
-
-Published 19/11/2024 AEST
 
 Welcome to my new blog! It's just being test out (for now) but I hope I can get it working!
---
+++ src/content/config.ts
@@ -0,0 +1,28 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// 2. Define your collection(s)
+const blogCollection = defineCollection({
+  type: "content", // v2.5.0 and later
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    description: z.string().optional(),
+    publishDate: z.date(),
+    author: z.string(),
+  }),
+});
+
+const projects = defineCollection({
+  type: "content", // v2.5.0 and later
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    image: z.string().optional(),
+  }),
+});
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  blog: blogCollection,
+  projects: projects,
+};
