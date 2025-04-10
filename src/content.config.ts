599196b84d9c5a405f9366cb7f31970573549b71
KTrain5369 <git@ktrain5369.me>
2025-04-10 15:38:10 +1000

    formatting + flesh out idea page layout

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,7 @@
 // 1. Import utilities from `astro:content`
 import { z, defineCollection, reference } from "astro:content";
-import { glob, file } from "astro/loaders"; 
-import { optional } from "astro:schema";
+import { glob, file } from "astro/loaders";
+
 // 2. Define your collection(s)
 const blogCollection = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
@@ -10,8 +10,8 @@ const blogCollection = defineCollection({
     tags: z.array(z.string()).optional(),
     description: z.string().optional(),
     publishDate: z.coerce.date(),
-    author: reference('authors'),
-    draft: z.boolean()
+    author: reference("authors"),
+    draft: z.boolean(),
   }),
 });
 
@@ -41,11 +41,15 @@ const authorCollection = defineCollection({
     description: z.string().optional(),
     pfp: z.string().optional(),
     written_posts: z.array(z.string()), // we reference these later
-    socials: z.array(z.object({
-      title: z.string(),
-      link: z.string()
-    })).optional(),
-  })
+    socials: z
+      .array(
+        z.object({
+          title: z.string(),
+          link: z.string(),
+        }),
+      )
+      .optional(),
+  }),
 });
 
 const groupCollection = defineCollection({
@@ -56,24 +60,30 @@ const groupCollection = defineCollection({
     pfp: z.string().optional(),
     tagline: z.string(),
     description: z.string(),
-    projects: z.array(z.object({
-      name: z.string(),
-      description: z.string(),
-      link: z.string()
-    })).optional(),
-    socials: z.array(z.object({
-      name: z.string(),
-      link: z.string()
-    }))
-  })
-})
+    projects: z
+      .array(
+        z.object({
+          name: z.string(),
+          description: z.string(),
+          link: z.string(),
+        }),
+      )
+      .optional(),
+    socials: z.array(
+      z.object({
+        name: z.string(),
+        link: z.string(),
+      }),
+    ),
+  }),
+});
 
 const ideaCollection = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/ideas" }),
   schema: z.object({
-    name: z.string()
-  })
-})
+    title: z.string(),
+  }),
+});
 
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
@@ -82,5 +92,5 @@ export const collections = {
   projects: projectCollection,
   authors: authorCollection,
   groups: groupCollection,
-  ideas: ideaCollection
+  ideas: ideaCollection,
 };
